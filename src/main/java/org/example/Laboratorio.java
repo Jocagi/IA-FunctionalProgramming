b1f4db49725e4fa2d1a65f5c728766f9844510ad
José Girón <42883411+Jocagi@users.noreply.github.com>
2021-02-15 16:10:57 -0600

    Laboratorio: Stream

--- src/main/java/org/example/Laboratorio.java
+++ src/main/java/org/example/Laboratorio.java
@@ -1,44 +1,29 @@
 package org.example;
 
+import java.math.BigInteger;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Laboratorio
 {
-    //Datos
     public static List<Integer> createRandomList(int qty)
     {
         var random = new Random();
         List<Integer> numbers = new LinkedList<>();
 
-        for (int i = 0; i<qty; i++)
+        for (int i = 0; i < qty; i++)
         {
             numbers.add(random.nextInt(100));
         }
         return numbers;
     }
 
-    public static List<Integer> sortList(List<Integer> unsortedList)
-    {
-        Collections.sort(unsortedList, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o1.compareTo(o2);
-            }
-        });
-        return unsortedList;
-    }
-
-    public static List<Integer> sortLambdaList(List<Integer> unsortedList)
-    {
-        Collections.sort(unsortedList, (o1, o2) ->  o2.compareTo(o1));
-        return unsortedList;
-    }
-
     public static boolean isPrimeNumber(int n)
     {
         if(n <= 1)
+        {
             return false;
+        }
 
         var contador = 0;
 
@@ -51,15 +36,15 @@ public class Laboratorio
         return contador <= 0;
     }
 
-    public static int fibonacci(int n)
+    public static BigInteger fibonacci(int n)
     {
-        int p1 = 0;
-        int p2 = 1;
-        int ans = 0;
+        BigInteger p1 = BigInteger.valueOf(0);
+        BigInteger p2 = BigInteger.valueOf(1);
+        BigInteger ans = BigInteger.valueOf(1);
 
-        for (int i = 1; i <= n; i++)
+        for (int i = 1; i < n; i++)
         {
-            ans = p1 + p2;
+            ans = p1.add(p2);
             p1 = p2;
             p2 = ans;
         }
@@ -68,35 +53,23 @@ public class Laboratorio
 
     public static void main(String args[])
     {
-        //int n = 11;
-        //System.out.println("Fibonnaci de " + n + " es " + fibonacci(n));
-
-
         System.out.println("Calculando Fibonacci de Primos...");
 
+        var value = Integer.valueOf(args[0]);
+
         var listadoPrimos =
-                createRandomList(Integer.valueOf(args[0])).parallelStream()
+                createRandomList(value).stream()
                 .filter(n -> isPrimeNumber(n))
                 .sorted()
                 .collect(Collectors.toList());
 
-        System.out.println("Calculo Finalizado");
-        System.out.println("Primer primo: " + listadoPrimos.get(0));
-        //System.out.println(listadoPrimos);
+        System.out.println("Numeros primos: " + listadoPrimos);
 
-
-        /*
-        var primos =
-                IntStream.range(0, 100).boxed()
-                .filter(n -> isPrimeNumber(n))
+        var listadoFibonacci = listadoPrimos.stream()
+                .map(n -> fibonacci(n))
                 .collect(Collectors.toList());
 
-        System.out.println(primos);
-
-        var randomNumbers = createRandomList(10);
-        System.out.println(randomNumbers);
-        sortLambdaList(randomNumbers);
-        System.out.println(randomNumbers);
-        */
+        System.out.println("Calculo Finalizado");
+        System.out.println("Resultado Fibonacci: " + listadoFibonacci);
     }
 }
