8a9bb07e23003dd085fda522c07c1b7d4a6d1602
José Girón <42883411+Jocagi@users.noreply.github.com>
2021-02-14 23:38:34 -0600

    Video 2: Clase anonima

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,15 +1,24 @@
 package org.example;
 
+import org.example.functional.MyFunctionalInterface;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
     public static void main( String[] args )
     {
-        var instanciaTux = new Tux();
+        //Clase anonima
+        var duke = new MyFunctionalInterface() {
+            @Override
+            public String doSomething(String param) {
+                return "Hola soy DUKE y he recibido: " + param;
+            }
+        };
 
-        System.out.println( instanciaTux.doSomething("Que divertido!") );
+        System.out.println(duke.doSomething("Clase anonima"));
     }
 }
