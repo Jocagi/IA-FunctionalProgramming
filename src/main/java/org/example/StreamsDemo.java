48a9cb8f6e374d3095f3c114506d138495ab33eb
José Girón <42883411+Jocagi@users.noreply.github.com>
2021-02-15 13:22:43 -0600

    Video 5: Prime Numbers

--- src/main/java/org/example/StreamsDemo.java
+++ src/main/java/org/example/StreamsDemo.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class StreamsDemo
 {
@@ -29,11 +30,50 @@ public class StreamsDemo
         return unsortedList;
     }
 
+    public static List<Integer> sortLambdaList(List<Integer> unsortedList)
+    {
+        Collections.sort(unsortedList, (o1, o2) ->  o2.compareTo(o1));
+        return unsortedList;
+    }
+
+    public static boolean isPrimeNumber(int n)
+    {
+        if(n <= 1)
+            return false;
+
+        var contador = 0;
+
+        for (int i = n - 1; i > 1; i--)
+        {
+            if(n % i == 0)
+                contador++;
+        }
+
+        return contador <= 0;
+    }
+
     public static void main(String args[])
     {
+        var listadoPrimos =
+                createRandomList(500_000_000).stream()
+                .filter(n -> isPrimeNumber(n))
+                .sorted()
+                .collect(Collectors.toList());
+
+        System.out.println(listadoPrimos);
+
+        /*
+        var primos =
+                IntStream.range(0, 100).boxed()
+                .filter(n -> isPrimeNumber(n))
+                .collect(Collectors.toList());
+
+        System.out.println(primos);
+
         var randomNumbers = createRandomList(10);
         System.out.println(randomNumbers);
-        sortList(randomNumbers);
+        sortLambdaList(randomNumbers);
         System.out.println(randomNumbers);
+        */
     }
 }
